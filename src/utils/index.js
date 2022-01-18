84b32f93f48921b029e8d14037276b4e9e0554c0
aoehiagwina <a.o.ehiagwina@gmail.com>
2022-01-18 14:50:55 +0000

    Front end first major push containing create, read and update functionality

---
+++ src/utils/index.js
@@ -0,0 +1,49 @@
+export const signUpFetch = async (username, email, password) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.console.log(error);
+    }
+  };
+  
+  
+  export const readUserFetch = async (username) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_REST_API}user/${username}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  
+  export const updateUserFetch = async (username, email) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+        }),
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
