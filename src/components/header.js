1cadd1af6af13087fc52919db0bb9f68aed1be98
QwuinJ <jackbelfield8@gmail.com>
2022-01-25 15:47:47 +0000

    header css, static hero img

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import '../css_component/header.css';
 
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
+	return (
+		<div className='header-wrapper'>
+			<div className='Header_Title'>
+				<h1>The Ultimate Challenge</h1>
+			</div>
 
-    return(
-        <div className="header">
-          
-            <div className="Header_Title">
-                <h1>The Ultimate Challenge</h1>
-                <h3>Where the best of gamers compete for the world championship</h3>
-            </div>
-
-            <div className="Header_Navs">
-                {/* <NavLink to='/home'>Home</NavLink> */}
-                {/* <div>
-                    
-                    {/* <NavLink to='/sign_up'>Sign Up</NavLink> */}
-                    {/* <NavLink to='/login'>Login</NavLink>
-                </div> */}
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/login'>Login</NavLink>
-                <NavLink to='/logout'>Logout</NavLink>
-                <NavLink to='/profile'>Profile</NavLink>
-                <NavLink to='/search'>Search</NavLink>
-            </div>
-
-
-        </div>
-    )
-}
+			<div className='Header_Navs'>
+				<NavLink to='/'>Home</NavLink>
+				<NavLink to='/login'>Login</NavLink>
+				<NavLink to='/logout'>Logout</NavLink>
+				<NavLink to='/profile'>Profile</NavLink>
+				<NavLink to='/search'>Search</NavLink>
+			</div>
+		</div>
+	);
+};
---
+++ src/components/hero_banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const HeroBanner = () => {
+	return (
+		<div className='hero-banner'>
+			<div className='hero-content'>
+				<h1>EXAMPLE TEST TEXT</h1>
+			</div>
+		</div>
+	);
+};
