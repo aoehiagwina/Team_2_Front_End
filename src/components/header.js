0cc761839e41c8f0e218944a024ab9d6c8986113
QwuinJ <jackbelfield8@gmail.com>
2022-01-26 10:57:23 +0000

    Revert "changed react 3"
    This reverts commit 37079f04fb3df5fdea3fb2ea168e32c366a74008.

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import '../css_component/header.css';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom"
 
 export const Header = () => {
-	return (
-		<div className='header-wrapper'>
-			<div className='Header_Title'>
-				<h1>The Ultimate Challenge</h1>
-			</div>
 
-			<div className='Header_Navs'>
-				<NavLink to='/'>Home</NavLink>
-				<NavLink to='/login'>Login</NavLink>
-				<NavLink to='/logout'>Logout</NavLink>
-				<NavLink to='/profile'>Profile</NavLink>
-				<NavLink to='/search'>Search</NavLink>
-			</div>
-		</div>
-	);
-};
+    return(
+
+        <div className="header">
+          
+            <div className="Header_Title">
+                <h1 className='major'>The Ultimate Challenge</h1>
+                <h3 className='minor'>Where the best of gamers compete for the world championship</h3>
+            </div>
+
+            <div className="Header_Navs">
+                <ul>
+                    <li> <NavLink to='/home'>Home</NavLink></li>
+                    <li><NavLink to='/login'>Login</NavLink></li>
+                    <li><NavLink to='/profile'>Profile</NavLink></li>
+                    <li><NavLink to='/search'>Search</NavLink></li>
+                </ul>
+                {/* <NavLink to='/home'>Home</NavLink> */}
+                {/* <div>
+                    
+                    {/* <NavLink to='/sign_up'>Sign Up</NavLink> */}
+                    {/* <NavLink to='/login'>Login</NavLink>
+                </div> */}
+                {/* <NavLink to='/'>Home</NavLink>
+                <NavLink to='/login'>Login</NavLink>
+                <NavLink to='/logout'>Logout</NavLink>
+
+                <NavLink to='/profile'>Profile</NavLink> */}
+
+                {/* <NavLink to='/profile'>Profile</NavLink>
+                <NavLink to='/search'>Search</NavLink> */}
+
+            </div>
+        </div>
+
+    )
+}
--- src/components/login.js
+++ src/components/login.js
@@ -1,76 +1,90 @@
-import '../App.css';
-import React, { useState } from 'react';
-import { signUpFetch, logInFetch } from '../utils';
+import "../App.css";
+import React, { useState } from "react";
+import { signUpFetch, logInFetch} from "../utils";
 import '../css_component/login.css';
 
 import '../css_component/sign_up.css';
 
-export const Login = ({ user, setUser }) => {
-	const [username, setUsername] = useState();
-	const [email, setEmail] = useState();
-	const [password, setPassword] = useState();
-	const [logBool, setLogBool] = useState(true);
+export const Login =({user, setUser}) =>{
+    //const [user, setUser] = useState();
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [logBool, setLogBool] = useState(true);
 
-	const submitHandler = (e) => {
-		e.preventDefault();
-		if (logBool) {
-			signUpFetch(username, email, password, setUser);
-		} else {
-			logInFetch(username, password, setUser);
-		}
-	};
 
-	return (
-		<div className='Login'>
-			{!user ? (
-				<div className='wrapper'>
-					<div className='form-signin-heading'>Join us today</div>
-					<form onSubmit={submitHandler} className='form-signin'>
-						<input
-							className='form-control'
-							onChange={(e) => setUsername(e.target.value)}
-							placeholder='Username'
-						/>
-						{logBool && (
-							<input
-								className='form-control'
-								onChange={(e) => setEmail(e.target.value)}
-								placeholder='Email'
-								type='email'
-							/>
-						)}
-						<input
-							className='form-control'
-							onChange={(e) => setPassword(e.target.value)}
-							placeholder='Password'
-							type='password'
-						/>
-						<label className='checkbox'>
-							<input
-								type='checkbox'
-								value='remember-me'
-								id='rememberMe'
-								name='rememberMe'
-							/>{' '}
-							Remember me
-						</label>
-						<button className='main_button' type='submit'>
-							<span>Register</span>
-						</button>
-					</form>
-					<button className='reg_btn' onClick={() => setLogBool(!logBool)}>
-						<span>
-							{logBool ? 'Do you want to log in?' : 'Do you want to sign up?'}
-						</span>
-					</button>
-				</div>
-			) : (
-				<div>
-					<h2>Welcome {user.username}</h2>
-				</div>
-			)}
-		</div>
-	);
-};
+    // useEffect(() => {
+    //   tokenCheck(setUser);
+    // }, []);
+    
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (logBool) {
+          signUpFetch(username, email, password, setUser);
+        } else {
+          logInFetch(username, password, setUser);
+        }
+      };
 
+      return(
+        <div className="Login">
+        {!user ? (
+          <div className="wrapper">
+             <div className="form-signin-heading">Join us today</div>
+            <form onSubmit={submitHandler} className="form-signin">
+              <input className="form-control"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
+              {logBool && (
+                <input className="form-control"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email" type="email"
+                />
+              )}
+              <input className="form-control"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password" type="password"
+              />
+              <label className="checkbox">
+                <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"/> Remember me
+              </label>
+              <button className="main_button" type="submit"><span>Register</span></button>
+            </form>
+            <button className="reg_btn" onClick={() => setLogBool(!logBool)}><span>
+                {logBool ? "Do you want to log in?" : "Do you want to sign up?"}
+              </span></button>
+             
+          </div>) : (<div >
+                    <h2  >Welcome {user.username}</h2>
+                
+                    </div>
+                  )}
+                </div>
+                
+              )}
+    
 export default Login;
+
+
+
+//   return(
+//         <div>
+//             <h4>Log in to compete for the ultimate gammers title</h4>
+//             <br/>
+//             <form onSubmit={login_Handler}>
+//                 <input
+//                 onChange={(e) => set_Username(e.target.value)}
+//                 placeholder="Enter Username"
+//                 />
+//                 <input
+//                 onChange={(e) => set_Password(e.target.value)}
+//                 placeholder="Enter password"
+//                 />
+//                 <button type="submit">Login</button>
+//             </form>
+//             <br/>
+//             <h5>Or {<Link to='/sign_up'>Sign Up </Link>}to begin your challenge</h5>
+//         </div>
+//     );
+// }
