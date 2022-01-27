7935d3b26d350eaaec4dd6f2ae2c66fa1275e404
QwuinJ <jackbelfield8@gmail.com>
2022-01-27 12:07:10 +0000

    css update

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../css_component/footer.css';
+
+export const Footer = () => {
+	return (
+		<div className='footer-wrapper'>
+			<div className='footer-content-box'>
+				<div className='footer-content'>
+					<img
+						className='footer-img'
+						src='../images/logo_white.png'
+						alt='ultimate challenge small logo'></img>
+				</div>
+
+				<div className='footer-content'>
+					<a
+						href='http://ultimatechallenge.netlify.app'
+						className='footer-link'>
+						The Ultimate Challenge
+					</a>
+					<br />
+					<a
+						href='http://ultimatechallenge.netlify.app'
+						className='footer-link'>
+						Where the best of gamers compete for the world
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,63 +1,78 @@
-import { Link } from "react-router-dom";
-import React, { useState, useEffect, useCallback} from 'react';
-import { deleteTournamentFetch, removePlayerFetch, searchTournamentFetch} from "../utils/tournament";
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+	deleteTournamentFetch,
+	removePlayerFetch,
+	searchTournamentFetch,
+} from '../utils/tournament';
 import { List } from './list';
 
-export const Profile = ({user}) => {
-    const [created, setCreated] = useState();
-    const [joined, setJoined] = useState();
-    
+export const Profile = ({ user }) => {
+	const [created, setCreated] = useState();
+	const [joined, setJoined] = useState();
 
-    const createdHandler = useCallback(() => {
-        if (user) {
-            searchTournamentFetch("creator", user._id, setCreated);
-        };
-    },[user]);
+	const createdHandler = useCallback(() => {
+		if (user) {
+			searchTournamentFetch('creator', user._id, setCreated);
+		}
+	}, [user]);
 
-    const joinedHandler = useCallback(() => {
-        if (user) {
-            searchTournamentFetch("players", user._id, setJoined);
-        };
-    },[user]);
+	const joinedHandler = useCallback(() => {
+		if (user) {
+			searchTournamentFetch('players', user._id, setJoined);
+		}
+	}, [user]);
 
-    useEffect( () => {
-        const fetchHandler = async () => {
-            await createdHandler();
-            await joinedHandler();
-        };
-        fetchHandler();
-    },[createdHandler, joinedHandler]);
+	useEffect(() => {
+		const fetchHandler = async () => {
+			await createdHandler();
+			await joinedHandler();
+		};
+		fetchHandler();
+	}, [createdHandler, joinedHandler]);
 
-    const deleteHandler = async (tournamentName) => {
-        await deleteTournamentFetch(tournamentName, user._id);
-        await createdHandler();
-    };
+	const deleteHandler = async (tournamentName) => {
+		await deleteTournamentFetch(tournamentName, user._id);
+		await createdHandler();
+	};
 
-    const leaveHandler = async (tournamentName) => {
-        await removePlayerFetch(tournamentName, user._id);
-        await joinedHandler();
-    };
+	const leaveHandler = async (tournamentName) => {
+		await removePlayerFetch(tournamentName, user._id);
+		await joinedHandler();
+	};
 
-    return (
-        <div>
-            {!user ? (
-                <div>
-                    <h2>Please log in</h2>
-                </div>
-            ) : (
-                <div>
-                    <h2>{user.username}'s Profile</h2>
-                    <div>
-                        <h2>Your tournaments</h2>
-                        {created && created.length > 0 && <List items={created} buttonName={"Delete"} handler={deleteHandler}/>}
-                    </div>
-                    <div>
-                        <h2>Joined tournaments</h2>
-                        {joined && joined.length > 0 && <List items={joined} buttonName={"Leave"} handler={leaveHandler}/>}
-                    </div>
-                    <p>{<Link to="/createTournament">Create a new tournament</Link>}</p>
-                </div>
-            )}
-        </div>
-    );
-}
+	return (
+		<div>
+			{!user ? (
+				<div className='profile-page'>
+					<h2>Please log in</h2>
+				</div>
+			) : (
+				<div>
+					<h2>{user.username}'s Profile</h2>
+					<div>
+						<h2>Your tournaments</h2>
+						{created && created.length > 0 && (
+							<List
+								items={created}
+								buttonName={'Delete'}
+								handler={deleteHandler}
+							/>
+						)}
+					</div>
+					<div>
+						<h2>Joined tournaments</h2>
+						{joined && joined.length > 0 && (
+							<List
+								items={joined}
+								buttonName={'Leave'}
+								handler={leaveHandler}
+							/>
+						)}
+					</div>
+					<p>{<Link to='/createTournament'>Create a new tournament</Link>}</p>
+				</div>
+			)}
+		</div>
+	);
+};
